10dae24a1e500715e75c0e7103aabbff93cc81b9
Jaewon Seo <powerofdeen@naver.com>
2023-01-17 17:25:47 +0900

    튜토리얼 진행

--- Scrumdinger/Models/Theme.swift
+++ Scrumdinger/Models/Theme.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-enum Theme: String {
+enum Theme: String, CaseIterable, Identifiable {
     case bubblegum
     case buttercup
     case indigo
@@ -35,8 +35,10 @@ enum Theme: String {
     var mainColor: Color {
         Color(rawValue)
     }
-    
     var name: String {
         rawValue.capitalized
+    }
+    var id: String {
+        name
     }
 }
